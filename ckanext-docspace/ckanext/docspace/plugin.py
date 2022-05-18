6ba2f142da9ab4f29b244b9977d8c4b208531de3
Ahmad Alobaid <ahmad88me@gmail.com>
2022-05-18 15:10:12 +0200

    adding sample button

--- ckanext-docspace/ckanext/docspace/plugin.py
+++ ckanext-docspace/ckanext/docspace/plugin.py
@@ -4,11 +4,27 @@ import ckan.plugins.toolkit as toolkit
 
 class DocspacePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IResourceView, inherit=True)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'docspace')
+        toolkit.add_resource('fanstatic', 'docspace')
+
+    # IResourceView
+    def info(self):
+        return {'preview_enabled': True, 'name': 'the button', 'title': 'The Buttonn'}
+
+    def can_view(self, data_dict):
+        return True
+
+    def setup_template_variables(self, context, data_dict):
+        return data_dict
+
+    def view_template(self, context, data_dict):
+        return 'button.html'
+
+    def form_template(self, context, data_dict):
+        return 'button.html'
+
