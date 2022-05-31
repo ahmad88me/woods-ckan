8b53fa87c385f174ac7dd3281aa7b53d6755dd25
Ahmad Alobaid <ahmad88me@gmail.com>
2022-05-31 13:19:57 +0200

    add custom field to the resource

--- ckanext-docspace/ckanext/docspace/plugin.py
+++ ckanext-docspace/ckanext/docspace/plugin.py
@@ -2,14 +2,59 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
-class DocspacePlugin(plugins.SingletonPlugin):
+class DocspacePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IDatasetForm)  # To add docspace field to the resources
+
     # plugins.implements(plugins.IResourceView, inherit=True)
     # plugins.implements(plugins.interfaces.IDatasetForm, inherit=True)
 
     # IConfigurer
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
+
+    def _modify_package_schema(self, schema):
+        # Add our custom country_code metadata field to the schema.
+        schema.update({
+            'country_code': [toolkit.get_validator('ignore_missing'),
+                             toolkit.get_converter('convert_to_tags')('country_codes')]
+        })
+        # Add our custom_test metadata field to the schema, this one will use
+        # convert_to_extras instead of convert_to_tags.
+        schema.update({
+            'custom_text': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        # Add our custom_resource_text metadata field to the schema
+        schema['resources'].update({
+            'custom_resource_text': [toolkit.get_validator('ignore_missing')]
+        })
+        return schema
+
+    def show_package_schema(self):
+        schema = super(DocspacePlugin, self).show_package_schema()
+
+        # Don't show vocab tags mixed in with normal 'free' tags
+        # (e.g. on dataset pages, or on the search page)
+        schema['tags']['__extras'].append(toolkit.get_converter('free_tags_only'))
+
+        # Add our custom country_code metadata field to the schema.
+        schema.update({
+            'country_code': [
+                toolkit.get_converter('convert_from_tags')('country_codes'),
+                toolkit.get_validator('ignore_missing')]
+            })
+
+        # Add our custom_text field to the dataset schema.
+        schema.update({
+            'custom_text': [toolkit.get_converter('convert_from_extras'),
+                toolkit.get_validator('ignore_missing')]
+            })
+
+        schema['resources'].update({
+                'custom_resource_text': [toolkit.get_validator('ignore_missing') ]
+            })
+        return schema
 
     # # IResourceView
     # def info(self):
