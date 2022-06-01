ff0814dc376b3ff75b0436726bc32e5c82ad6a21
Ahmad Alobaid <ahmad88me@gmail.com>
2022-06-01 20:39:41 +0200

    comment

--- ckanext-docspace/ckanext/docspace/plugin.py
+++ ckanext-docspace/ckanext/docspace/plugin.py
@@ -125,15 +125,15 @@ def get_table_content():
 
 def add_update_docspace(context, data_dict):
     sss = SSSAPIS(username="aalobaid@fi.upm.es", password="JxD7Z7>r!b!U/RJ")
-    if data_dict['docspace_viewid'].strip() == "":
-        # create
-        table = get_table_content()
-        sss.create_private_view(table=table)
-    else:
-        # update
-        table = get_table_content()
-        sss.update_view(view_id=data_dict['docspace_viewid'], table=table)
-        # ckan.logic.action.update.resource_update(context, {'docspace_viewid'})
+    # if data_dict['docspace_viewid'].strip() == "":
+    #     # create
+    #     table = get_table_content()
+    #     sss.create_private_view(table=table)
+    # else:
+    #     # update
+    #     table = get_table_content()
+    #     sss.update_view(view_id=data_dict['docspace_viewid'], table=table)
+    #     # ckan.logic.action.update.resource_update(context, {'docspace_viewid'})
     return {
         "context": str(context),
         "data_dict": str(data_dict)
