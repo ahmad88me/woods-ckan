b792de64ec79ac68cac79818d0bbf011f93ad251
Ahmad Alobaid <ahmad88me@gmail.com>
2022-06-01 20:38:15 +0200

    add create/update

--- ckanext-docspace/ckanext/docspace/plugin.py
+++ ckanext-docspace/ckanext/docspace/plugin.py
@@ -119,32 +119,25 @@ class DocspacePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     #     return '/package/resource_read.html'
 
 
-# def get_table_content():
-#     return [["A", "B", "C"]]
+def get_table_content():
+    return [["A", "B", "C"]]
 
 
 def add_update_docspace(context, data_dict):
+    sss = SSSAPIS(username="aalobaid@fi.upm.es", password="JxD7Z7>r!b!U/RJ")
+    if data_dict['docspace_viewid'].strip() == "":
+        # create
+        table = get_table_content()
+        sss.create_private_view(table=table)
+    else:
+        # update
+        table = get_table_content()
+        sss.update_view(view_id=data_dict['docspace_viewid'], table=table)
+        # ckan.logic.action.update.resource_update(context, {'docspace_viewid'})
     return {
-        # "context": str(context),
+        "context": str(context),
         "data_dict": str(data_dict)
     }
-
-    # sss = SSSAPIS(username="aalobaid@fi.upm.es", password="JxD7Z7>r!b!U/RJ")
-    # if data_dict['docspace_viewid'].strip() == "":
-    #     pass
-        # create
-    #     table = get_table_content()
-    #     sss.create_private_view(table=table)
-    #     #
-    # else:
-    #     # update
-    #     table = get_table_content()
-    #     sss.update_view(view_id=data_dict['docspace_viewid'], table=table)
-    #     # ckan.logic.action.update.resource_update(context, {'docspace_viewid'})
-    # return {
-    #     "context": str(context),
-    #     "data_dict": str(data_dict)
-    # }
     # with open("/home/woods/docspace_logtest", 'w') as f:
     #     f.write(str(datetime.now()))
     #     f.write('\n')
