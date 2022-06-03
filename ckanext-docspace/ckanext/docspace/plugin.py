88b4ae9ddd2bd28be7529007bc094d911cde0769
Ahmad Alobaid <ahmad88me@gmail.com>
2022-06-03 16:16:11 +0200

    save

--- ckanext-docspace/ckanext/docspace/plugin.py
+++ ckanext-docspace/ckanext/docspace/plugin.py
@@ -163,7 +163,7 @@ def add_update_docspace(context, data_dict):
     try:
         sss = SSSAPIS(token="bcc51260d15948ffb9346feee3d01358")
         print("ABC ... ")
-        domain_url = ckan.site_url
+        domain_url = config['ckan.site_url']
         print(os.environ['CKAN_CONFIG'])
         print(str(config))
         # print(CONFIG_FROM_ENV_VARS['ckan.site_url'])
